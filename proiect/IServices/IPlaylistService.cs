7f95e746960ab27df5b73fb30275cae197fde0c7
Dragomir Ioana <49548253+Ioana2499@users.noreply.github.com>
2022-01-24 22:36:59 +0200

    Add files via upload

---
+++ proiect/IServices/IPlaylistService.cs
@@ -0,0 +1,16 @@
+﻿using Playlist_Manager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Playlist_Manager.IServices
+{
+    public interface IPlaylistService
+    {
+        List<PlaylistModel> GetAllPlaylists();
+        void CreatePlaylist(PlaylistModel playlist);
+        void UpdatePlaylist(PlaylistModel playlist);
+        void DeletePlaylist(int id);
+    }
+}
