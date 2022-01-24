de32f6ac05b7cdb18acdfce691c058ac0e7d033d
Dragomir Ioana <49548253+Ioana2499@users.noreply.github.com>
2022-01-24 22:38:22 +0200

    Add files via upload

---
+++ proiect/Models/PlaylistModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Playlist_Manager.Models
+{
+    public class PlaylistModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<SongModel> SongList { get; set; }
+    }
+}
---
+++ proiect/Models/SongModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Playlist_Manager.Models
+{
+    public class SongModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Artist { get; set; }
+    }
+}
